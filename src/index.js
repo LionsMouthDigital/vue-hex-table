0cdc2d84ef6252e534afd2f379dd08b958ae0e68
Curtis Blackwell <blackwell.curtis@gmail.com>
2016-04-07 16:38:18 +0900

    :sparkles: webpack

--- src/index.js
+++
@@ -1,4 +0,0 @@
-exports.HexSuperTable       = require('./components/HexSuperTable.vue');
-exports.HexSuperTableFilter = require('./components/HexSuperTableFilter.vue');
-exports.HexTable            = require('./components/HexTable.vue');
-exports.HexTableToJson      = require('./components/HexTableToJson.vue');
